78a32cb149bc17b35420dee892e3a4944d01d3aa
santosh <santosh.ece06@gmail.com>
2020-01-12 11:57:14 +0530

    init.

---
+++ TODO.md
@@ -0,0 +1,16 @@
+# TODO
+
+- Create Project
+- Run Project
+- On Emulator Ctrl + M
+- Handling the data type error in VSCODE
+
+```
+    You need to create .vscode folder and settings.json
+    "javascript.validate.enable": false
+```
+
+- Create Your First React File. (Where to create your program)
+- Write Your First Component
+- Integrate with index.js
+- Inline Styling :: using {{}}
---
+++ src/Hello.js
@@ -0,0 +1,25 @@
+import React from 'react'; // core react :: Defaul
+import {View, Text} from 'react-native';
+
+/** Creating First Component */
+export function Hello() {
+  const appTitle = 'Kharghar';
+
+  // JSX SYNTAX
+  return (
+    <View>
+      <Text style={{fontSize: 30, color: 'white', backgroundColor: 'gold'}}>
+        Hello World
+      </Text>
+      <Text>{appTitle}</Text>
+    </View>
+  );
+}
+
+/**
+ *  JSX ()
+ *  AND {}
+ */
+
+/** Fat Arrow Functions */
+export const World = () => {};
